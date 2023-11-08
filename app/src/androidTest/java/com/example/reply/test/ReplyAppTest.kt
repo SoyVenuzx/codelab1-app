1d8c246d15854b08448677353061a88f7bae4e60
SoyVenuzx <ervinalvarezrodriguez@gmail.com>
2023-11-07 21:24:20 -0600

    First commit

---
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppTest.kt
@@ -0,0 +1,10 @@
+package com.example.reply.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import org.junit.Rule
+
+class ReplyAppTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+}
